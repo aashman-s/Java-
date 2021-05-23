ea3784da6d2e9bf8e3b359b635f612c20fd2cf87
aashmansrivastava <aashmansrivastava@localhost>
2021-05-23 13:38:20 +0800

    hello world
    intial commit made with hello world logic

---
+++ src/javatest/javaclass.java
@@ -0,0 +1,18 @@
+package javatest;
+
+import java.util.Scanner;
+
+public class javaclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Scanner sc = new Scanner(System.in);
+//		
+//		String name = sc.next();
+		
+		System.out.println("hello " + args[1] );
+
+	}
+
+}
